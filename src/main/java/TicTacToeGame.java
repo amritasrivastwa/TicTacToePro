cac70aff78c68d2e1043da348ed52d400c1a4183
AMRITA <amritagolu678@gmail.com>
2020-09-25 21:13:18 +0530

    Verified TicTacToeGame to return X

---
+++ src/main/java/TicTacToeGame.java
@@ -0,0 +1,6 @@
+public class TicTacToeGame {
+
+    public char getPlayer() {
+        return 'X';
+    }
+}
---
+++ src/test/java/TicTacToeGameTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.lang.annotation.Target;
+
+public class TicTacToeGameTest {
+
+    @Test
+    public void shouldReturnXOnFirstTurn(){
+
+        TicTacToeGame ticTacToeGame = new TicTacToeGame();
+        Assert.assertEquals('X',ticTacToeGame.getPlayer());
+    }
+}
