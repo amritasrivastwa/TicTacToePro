a2e57b7ca79412565cbaa5eb1bbf699fa611ba4c
AMRITA <amritagolu678@gmail.com>
2020-09-25 22:06:34 +0530

    Extracted static variable for player X and O

--- src/main/java/TicTacToeGame.java
+++ src/main/java/TicTacToeGame.java
@@ -1,9 +1,12 @@
 public class TicTacToeGame {
 
+    public static final char PLAYER_X = 'X';
+    public static final char PLAYER_O = 'O';
+
     public char getPlayer(int slotNumber) {
         if (slotNumber == 1)
-            return 'X';
+            return PLAYER_X;
         else
-            return 'O';
+            return PLAYER_O;
     }
 }
--- src/test/java/TicTacToeGameTest.java
+++ src/test/java/TicTacToeGameTest.java
@@ -2,23 +2,25 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.lang.annotation.Target;
-
 public class TicTacToeGameTest {
 
+    public static final char PLAYER_X = 'X';
+    public static final char PLAYER_O = 'O';
     private TicTacToeGame ticTacToeGame;
+
     @Before
     public void init() {
         ticTacToeGame = new TicTacToeGame();
     }
+
     @Test
-    public void shouldReturnXOnFirstTurn(){
-        Assert.assertEquals('X',ticTacToeGame.getPlayer(1));
+    public void shouldReturnXOnFirstTurn() {
+        Assert.assertEquals(PLAYER_X, ticTacToeGame.getPlayer(1));
     }
 
     @Test
-    public void shouldReturnOonSecondTurn(){
-        Assert.assertEquals('O',ticTacToeGame.getPlayer(2));
+    public void shouldReturnOonSecondTurn() {
+        Assert.assertEquals(PLAYER_O, ticTacToeGame.getPlayer(2));
     }
 
 
